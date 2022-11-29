ed539dda7a3f2f16d6c0bfc79f4bd6d3fd0e7778
mukotso <kelvinmukotso@gmail.com>
2022-11-29 07:42:35 +0300

    initial app set-up and basic WorldData and footer component without content

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import WorldData from './WorldCountriesData';
+import Footer from './common/Footer'
+import React from "react";
+
+
+ const app = (
+    <div  className='app'>
+        <>
+            <WorldData/>
+            <Footer/>
+        </>
+    </div>
+)
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(App, rootElement)
