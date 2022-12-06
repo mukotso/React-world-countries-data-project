76dda6521f85b43d7c44b300c0b35ba8c4303dcd
mukotso <kelvinmukotso@gmail.com>
2022-12-06 08:43:21 +0300

    added analytics components the top ten populated countries and most spoken languages

--- src/components/WorldCountriesData.js
+++ src/components/WorldCountriesData.js
@@ -1,40 +1,55 @@
 import React, {useEffect, useState} from 'react';
 import Header from './Header'
+import Population from "./Population";
+import Languages from "./Languages";
 import axios from "axios";
-const WorldCountriesData = () => {
-    const [allCountriesData , setAllCountriesData] = useState([]);
-    const [countriesData , setCountriesData] = useState([]);
-    const [searchTerm , setSearchTerm] = useState('');
 
-    useEffect(()=>{
+const WorldCountriesData = () => {
+    const [allCountriesData, setAllCountriesData] = useState([]);
+    const [countriesData, setCountriesData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [showPopulation, setShowPopulation] = useState(true);
+    const [showLanguages, setShowLanguages] = useState(false);
+
+    useEffect(() => {
         fetchCountriesData();
     }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         searchCountry();
-    },[searchTerm])
+    }, [searchTerm])
 
-   const  fetchCountriesData =  ()=>{
+    const fetchCountriesData = () => {
         axios.get('https://restcountries.com/v2/all')
-            .then((response)=>{
-                const data =  response.data
+            .then((response) => {
+                const data = response.data
                 setCountriesData(data)
                 setAllCountriesData(data)
             })
 
     }
 
-    const searchCountry = () =>{
-        axios.get('https://restcountries.com/v2/name/'+searchTerm)
-         .then((response)=>{
-            const data =  response.data
-            setCountriesData(data)
-        })
+    const searchCountry = () => {
+        axios.get('https://restcountries.com/v2/name/' + searchTerm)
+            .then((response) => {
+                const data = response.data
+                setCountriesData(data)
+            })
     }
 
-   function  handleChange(e){
+    function handleChange(e) {
         const value = e.target.value;
         setSearchTerm(value)
+    }
+
+    function toggleShowPopulation(){
+        setShowPopulation(true);
+        setShowLanguages(false)
+    }
+
+    function toggleShowLanguages(){
+        setShowPopulation(false);
+        setShowLanguages(true)
     }
 
 
@@ -42,8 +57,8 @@ const WorldCountriesData = () => {
         <>
             <Header countriesData={allCountriesData}/>
             <center>
-                <input  className='search-input' type={'text'} placeholder={'Search countries by name'}
-                        onChange={handleChange}
+                <input className='search-input' type={'text'} placeholder={'Search countries by name'}
+                       onChange={handleChange}
                 />
 
             </center>
@@ -51,17 +66,17 @@ const WorldCountriesData = () => {
             <div className={'country-card-wrapper'}>
 
                 {countriesData.map((country) => {
-                    const {name , flag, population , capital,languages } = country;
+                    const {name, flag, population, capital, languages} = country;
 
                     return (
                         <div className={'country-card'}>
-                            <img style={{width:'100%' , height:180}} src={flag} alt={'country-flag'}/>
+                            <img style={{width: '100%', height: 180}} src={flag} alt={'country-flag'}/>
                             <h2> Name : {name} <br/>
                                 Capital : {capital} <br/>
                                 {/*Languages:  {languageNames} <br/>*/}
                                 Population: {population} <br/>
                                 {/*/!*currency: {country.currencies.symbol} */}
-                                </h2>
+                            </h2>
                         </div>
                     )
                 })
@@ -69,6 +84,14 @@ const WorldCountriesData = () => {
                 }
 
             </div>
+
+            <center style={{marginTop:50}}>
+                <button onClick={toggleShowPopulation} className={'btn-yellow'}>POPULATION</button>
+                <button onClick={toggleShowLanguages}  className={'btn-yellow'}>LANGUAGES</button>
+            </center>
+
+            {showPopulation && <Population data={allCountriesData}/> }
+            {showLanguages &&  <Languages data={allCountriesData}/> }
         </>
     )
 
