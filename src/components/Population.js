a2f2424269d65242200397a1b5753d9e8f3e9728
mukotso <kelvinmukotso@gmail.com>
2022-12-16 10:31:06 +0300

    show countries by the alpah#Code instead of name

--- src/components/Population.js
+++ src/components/Population.js
@@ -1,13 +1,30 @@
 import React from 'react';
 const Population = (props)=>{
-    const topTenPopulatedCountries = props.data.sort((a,b)=> b.population - a.population).slice(0, 10);
+    console.log(props.data);
+    let topTenPopulatedCountries = props.data.sort((a,b)=> b.population - a.population).slice(0, 10);
+    let worldPopulation = 0;
+    topTenPopulatedCountries.forEach((country)=>{
+        worldPopulation += country.population;
+    })
+    let world={
+        alpha3Code:'World',
+        population:worldPopulation,
+    }
+    topTenPopulatedCountries.unshift(world)
+
+
+    const width = (population)=>{
+        let widthPercentage=   (population / worldPopulation ) *100
+        return widthPercentage+'%';
+    }
     return(
         <>
             {topTenPopulatedCountries.map(country=>{
+                let populationBarWidth = width(country.population)
                 return <div className={'population-container'}>
-                    <b className={'country-name'}>{country.name}</b>
-                    <span className={'population-bar'}></span>
-                    {country.population}
+                    <div className={'country-name'}>{country.alpha3Code}</div>
+                    <div style={{width:populationBarWidth}} className={'population-bar'}></div>
+                   <div className={'population'}> {country.population}</div>
                 </div>
             })}
         </>
